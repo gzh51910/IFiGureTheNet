d455d4f8764c91c44c2635efc85e82f69e88bd64
wyh <1398208538@qq.om>
2019-11-27 21:40:22 +0800

    搭建布局

--- WoTu/wotu/src/App.js
+++ WoTu/wotu/src/App.js
@@ -1,13 +1,83 @@
-import React from 'react';
-
+import React, { Component } from 'react';
+import { Route, Redirect, Switch,withRouter } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home'
+import List from './pages/List'
+import SettingWall from './pages/SettingWall'
+import Login from './pages/Login'
+import Reg from './pages/Reg'
+import Goods from './pages/Goods'
+import Mine from './pages/Mine'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>我图网</h1>
+import { Menu, Icon } from 'antd';
+// import 'antd/dist/antd.css';
+class App extends Component{
+  state = {
+       menu: [{
+           name: 'home',
+           path: '/home',
+           icon: 'home',
+           text: '首页'
+         }, {
+           name: 'list',
+           path: '/list',
+           icon: 'compass',
+           text: '分类'
+         },
+         {
+           name: 'settingwall',
+           path: '/settingwall',
+           icon: 'shopping-cart',
+           text: '背景墙'
+         }, {
+           name: 'mine',
+           path: '/mine',
+           icon: 'user',
+           text: '我的'
+         },
+       ]
+  }
+  goto = ({ key: path }) => {
+        let { history } = this.props;
+        this.setState({
+            currentPath: path
+        })
+        history.push(path)
+    }
+  
+  render() {
+    return (
+      <div>
+       <Menu
+            onClick={this.goto}
+            // selectedKeys={this.state.currentPath}
+            mode="horizontal"
+            theme='dark'
+        >
+            {
+                this.state.menu.map(item => {
+                return  <Menu.Item key={item.path}>
+                          <Icon type={item.icon}/>
+                          {item.text}
+                     </Menu.Item>
+                })
+        }
+        </Menu>
+           <Switch>
+              <Route path="/home" component={Home}/>
+              <Route path="/list" component={List}/>
+              <Route path="/settingwall" component={SettingWall} />
+              <Route path="/mine" component={Mine}/>
+              <Route path="/reg" component={Reg}/>
+              <Route path="/login" component={Login}/>
+              <Route path="/goods/:id" component={Goods} />
+              <Route path="/notfound" render={()=><div>404页面</div>}/>
+              <Redirect from="/" to="/home" exact/>
+              <Redirect to="/notfound"/>
+          </Switch> 
     </div>
-  );
+   )
+  }
 }
-
+App = withRouter(App);
 export default App;
---
+++ WoTu/wotu/src/pages/Goods.js
@@ -0,0 +1,12 @@
+
+
+import React, { Component } from 'react'
+class Login extends Component {
+    render() {
+        return (
+           
+                <h1>商品列表</h1>
+        )
+    }
+}
+export default Login;
